1235bc976c68aa167315787e0493033dedd5379c
Vilizar Denichin <denichinv@gmail.com>
2024-12-08 17:28:37 +0000

    buttons added

--- my-project/src/JobBoard.jsx
+++ my-project/src/JobBoard.jsx
@@ -1,19 +1,25 @@
 import  { useState } from "react";
 
 const JobBoard = () => {
-  const companyName = "Jobs.co.uk";
-  const [jobCount, setJobCount] = useState(Math.floor(Math.random() * 10)); 
+  const companyName = "\"Task Handlers\"";
+  const [taskCount, setTaskCount] = useState(Math.floor(Math.random() * 10)); 
 
   const getJobMessage = () => {
-    if (jobCount === 0) {
-      return "No jobs to schedule today!";
+    if (taskCount === 0) {
+      return "No tasks to schedule today!";
     } else {
-      return `Jobs running today from bot: ${jobCount}`;
+      return `Tasks running today from bot: ${taskCount}`;
     }
   };
 
-  const incrementJobCount = () => {
-    setJobCount((prevCount) => prevCount + 1); 
+  const incrementTaskCount = () => {
+    setTaskCount(taskCount + 1); 
+  };
+  const decrementTaskCount = () => {
+    setTaskCount(taskCount - 1); 
+  };
+  const resetTaskCount = () => {
+    setTaskCount(0); 
   };
 
   return (
@@ -22,9 +28,21 @@ const JobBoard = () => {
       <p>{getJobMessage()}</p>
       <button
         style={{ color: "red" }}
-        onClick={incrementJobCount} 
+        onClick={incrementTaskCount} 
       >
-        Create a job
+        Create task
+      </button>
+      <button
+        style={{ color: "red" }}
+        onClick={decrementTaskCount} 
+      >
+       Remove task
+      </button>
+      <button
+        style={{ color: "red" }}
+        onClick={resetTaskCount} 
+      >
+        Reset task
       </button>
     </div>
   );
